c2e5512cd1c4a98c6138ce9575be2b4438479201
Mateus Moreira <mateusmoreira024@gmail.com>
2021-09-28 15:40:13 -0300

    arquivos da urna

---
+++ js/etapas.js
@@ -0,0 +1,112 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '36111',
+                nome: 'Isis Valverde',
+                partido: 'GLOBAL',
+                fotos:[
+                    {url:'image/cv1.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '25941',
+                nome: 'Dread Hot',
+                partido: 'PMN',
+                fotos:[
+                    {url:'image/cv2.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '44121',
+                nome: 'Chico Pobre',
+                partido: 'PVTC',
+                fotos:[
+                    {url:'image/cv3.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '51333',
+                nome: 'Eddie Cabeleira',
+                partido: 'PPJ',
+                fotos:[
+                    {url:'image/cv4.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '93111',
+                nome: 'Eddie Careca',
+                partido: 'PBM',
+                fotos:[
+                    {url:'image/cv5.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '77355',
+                nome: 'Matrin Peludo',
+                partido: 'PDCT',
+                fotos:[
+                    {url:'image/cv6.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '36',
+                nome:'Bruno Brisa Forte',
+                partido: 'FLW',
+                vice:"Oseas",
+                fotos:[
+                    {url:'image/cp1.jpg', legenda: 'Prefeito', small: false},
+                    {url:'image/v1.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '25',
+                nome:'Carrinha Furacão',
+                partido: 'PMN',
+                vice:"Cicero",
+                fotos:[
+                    {url:'image/cp2.jpg', legenda: 'Prefeito', small: false},
+                    {url:'image/v2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '44',
+                nome:'Crivella Arrombado',
+                partido: 'PNC',
+                vice:"Jurandir",
+                fotos:[
+                    {url:'image/cp3.jpg', legenda: 'Prefeito', small: false},
+                    {url:'image/v3.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '93',
+                nome:'Padeiro Manoel',
+                partido: 'PQP',
+                vice:"Eulalia",
+                fotos:[
+                    {url:'image/cp4.jpg', legenda: 'Prefeito', small: false},
+                    {url:'image/v4.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '77',
+                nome:'Everson Zoio',
+                partido: 'PVSF',
+                vice:"Milton",
+                fotos:[
+                    {url:'image/cp1.jpg', legenda: 'Prefeito', small: false},
+                    {url:'image/v1.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    }
+];
---
+++ js/func.js
@@ -0,0 +1,143 @@
+let num = document.querySelector('.info-voto');
+let cargo = document.querySelector('.info-2 span');
+let aviso = document.querySelector('.src-2');
+let fotos = document.querySelector('.foto');
+let votoInfo = document.querySelector('.info-1 span');
+let descricao = document.querySelector('.info-dados');
+
+
+let etapaAtual = 0;
+let numero = '';
+let votoBranco = false;
+let votos = [];
+
+
+function comecaEtapa(){
+    let etapa = etapas[etapaAtual];
+    let numHTML = '';
+    numero = '';
+    votoBranco = false;
+
+    for(let i = 0; i < etapa.numeros; i++){
+        if(i === 0){
+            numHTML += '<div class="numero pisca"></div>';
+        }
+        else{
+            numHTML += '<div class="numero"></div>';
+        }
+    }
+
+    votoInfo.style.diplay = 'none';
+    cargo.innerHTML = etapa.titulo;
+    fotos.innerHTML = '';
+    descricao.innerHTML = '';
+    aviso.style.display = 'none';
+    num.innerHTML = numHTML;
+}
+
+function atualizaUi(){
+    let etapa = etapas[etapaAtual];
+    let candidato = etapa.candidatos.filter((item) =>{
+        if(item.numero === numero){
+            return true;
+        }
+        else{
+            return false;
+        }
+    });
+
+    if(candidato.length > 0){
+        candidato = candidato[0];
+
+        votoInfo.style.display = 'block';
+        descricao.innerHTML = `Nome: ${candidato.nome}<br/> Partido ${candidato.partido}`;
+        aviso.style.display = 'block';
+
+        let fotosHTML = '';
+        for(let i = 0; i < candidato.fotos.length; i++) {
+            if(i === 0){
+                fotosHTML += `<div class="image"><img src="${candidato.fotos[i].url}" alt="">${candidato.fotos[i].legenda}</div>`;
+            }
+            else{
+                fotosHTML += `<div class="image small"><img src="${candidato.fotos[i].url}" alt="">${candidato.fotos[i].legenda}</div>`;
+            }
+        }
+        fotos.innerHTML = fotosHTML;
+    }
+    else{
+        votoInfo.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = '<div class="aviso-grande pisca">VOTO NULO</div>'
+    }
+
+}
+
+function clicou(n){
+    let elNumero = document.querySelector('.numero.pisca');
+    if(elNumero !== null){
+        elNumero.innerHTML = n;
+        numero = `${numero}${n}`;
+
+        elNumero.classList.remove('pisca');
+        if(elNumero.nextElementSibling !== null){
+            elNumero.nextElementSibling.classList.add('pisca');
+        }else{
+            atualizaUi();
+        }
+    }
+}
+
+
+function branco(){
+    numero = '';
+    votoBranco = true;
+    votoInfo.style.display = 'block';
+    aviso.style.display = 'block';
+    num.innerHTML = '';
+    fotos.innerHTML = '';
+    descricao.innerHTML = '<div class="aviso-grande pisca">VOTO EM BRANCO</div>';
+}
+
+function corrige(){
+    comecaEtapa();
+}
+
+
+function confirma(){
+    let etapa = etapas[etapaAtual];
+    let votoConfirmado = false;
+
+
+    if(votoBranco === true){
+        votoConfirmado = true;
+        votos.push(
+            {
+                etapa: etapas[etapaAtual].titulo,
+                voto: 'branco'
+            }
+        );
+    }
+    else if(numero.length === etapa.numeros){
+        votoConfirmado = true;
+        votos.push(
+            {
+                etapa: etapas[etapaAtual].titulo,
+                voto: numero
+            }
+        );
+    }
+
+    if(votoConfirmado){
+        etapaAtual++;
+        if(etapas[etapaAtual] !== undefined){
+            comecaEtapa();
+        }
+        else{
+            document.querySelector('.tela').innerHTML = '<div class="aviso-gigante pisca">FIM</div>';
+            console.log(votos);
+        }
+    }
+}
+
+
+comecaEtapa();
